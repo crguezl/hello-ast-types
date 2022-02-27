478ced6717758e45c4e1bbe363682a1ad6ab348a
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2022-02-27 20:55:28 +0000

    scope example

---
+++ scope.js
@@ -0,0 +1,26 @@
+import { namedTypes as n, builders as b, visit } from "ast-types";
+import recast from "recast";
+import flow from  "flow-parser";
+const deb = x => (JSON.stringify(x, null,2));
+
+const code = `
+const a = 4;
+
+function tutu(a) {
+    const b = a+1;
+    return b;
+}
+`;
+
+let ast = flow.parse(code);
+
+visit(ast, {
+
+    visitVariableDeclarator(path) {
+        const node = path.node;
+        console.log(node.id.name, path.scope.isGlobal, path.scope.depth);
+
+        this.traverse(path)
+    }
+})
+
