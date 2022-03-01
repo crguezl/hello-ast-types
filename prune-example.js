200197110ddb2405de362ec74175ce9459f10d22
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2022-03-01 13:18:15 +0000

    prune-example.js

---
+++ prune-example.js
@@ -0,0 +1,42 @@
+//import  * as espree from "espree";
+import { parse, Syntax } from "espree";
+import {
+    Type,
+    namedTypes as n,
+    builders as b,
+    Path,
+    NodePath,
+    PathVisitor,
+    builtInTypes as builtin,
+    use,
+    getSupertypeNames,
+    getFieldValue,
+    eachField,
+    visit,
+    defineMethod,
+    astNodesAreEquivalent,
+  } from "ast-types";
+
+  const deb = x => (JSON.stringify(x, null, 2));
+
+  var programPath = new NodePath(parse("var y = 1,x = 2;"));
+  
+  var variableDeclaration = programPath.get("body", 0); 
+  // { ... declarations: [ VariableDeclarator, VariableDeclarator], ... }
+  
+  var yVariableDeclaratorPath = variableDeclaration.get("declarations", 0);
+  var xVariableDeclaratorPath = variableDeclaration.get("declarations", 1);
+
+  var remainingNodePath = yVariableDeclaratorPath.prune(); // returns the closest parent NodePath
+  remainingNodePath = xVariableDeclaratorPath.prune();
+
+  console.log(deb(programPath.node)); 
+  /*
+  {
+  "type": "Program",
+  "start": 0,
+  "end": 16,
+  "body": [],
+  "sourceType": "script"
+}
+*/
