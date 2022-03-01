7b0ae3542a309f8b56052eb07058167191186396
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2022-03-01 14:51:18 +0000

    scope-catch.js

---
+++ scope-catch.js
@@ -0,0 +1,51 @@
+import assert from "assert";
+import { parse, Syntax } from "espree";
+import { namedTypes as n, NodePath,} from "ast-types";
+
+const deb = (x) => JSON.stringify(x, null, 2);
+
+// "catch block scope"
+var catchWithVarDecl = `
+  function foo(e) {
+    try {
+      bar();
+    } catch (e) {
+      var f = e + 1;
+      return function(g) {
+        return e + g;
+      };
+    }
+    return f;
+  }
+`;
+
+var path = new NodePath(parse(catchWithVarDecl));
+var fooPath = path.get("body", 0);
+var fooScope = fooPath.scope;
+var catchPath = fooPath.get("body", "body", 0, "handler");
+var catchScope = catchPath.scope;
+
+// it should not affect outer scope declarations
+n.FunctionDeclaration.assert(fooScope.node);
+assert.strictEqual(fooScope.declares("e"), true);
+assert.strictEqual(fooScope.declares("f"), true);
+assert.strictEqual(fooScope.lookup("e"), fooScope);
+
+//it should declare only the guard parameter
+n.CatchClause.assert(catchScope.node);
+assert.strictEqual(catchScope.declares("e"), true);
+assert.strictEqual(catchScope.declares("f"), false);
+assert.strictEqual(catchScope.lookup("e"), catchScope);
+assert.strictEqual(catchScope.lookup("f"), fooScope);
+
+//it("should shadow only the parameter in nested scopes", function() {
+var closurePath = catchPath.get("body", "body", 1, "argument");
+var closureScope = closurePath.scope;
+n.FunctionExpression.assert(closureScope.node);
+assert.strictEqual(closureScope.declares("e"), false);
+assert.strictEqual(closureScope.declares("f"), false);
+assert.strictEqual(closureScope.declares("g"), true);
+assert.strictEqual(closureScope.lookup("g"), closureScope);
+assert.strictEqual(closureScope.lookup("e"), catchScope);
+assert.strictEqual(closureScope.lookup("f"), fooScope);
+
