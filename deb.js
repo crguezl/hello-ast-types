1f5cd4270bdd5e8f0c0c8987d751b8d9c97dbf7b
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2022-03-08 15:15:29 +0000

    deb.js

--- deb.js
+++ deb.js
@@ -2,6 +2,6 @@ import cj from "color-json";
 
 const SkippedKeys = new Set(["start", "end", "raw", "sourceType"]);
 const skip = (key, value) => SkippedKeys.has(key)? undefined : value;
-const deb = (x) => cj(JSON.stringify(x, skip, 2));
+const deb = x => cj(JSON.stringify(x, skip, 2));
 
 export default deb;
