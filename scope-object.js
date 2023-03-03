66166817e50273fb23492ea84cc9e139bc7e3855
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2023-03-03 17:10:54 +0000

    scope-object.js

--- scope-object.js
+++ scope-object.js
@@ -1,52 +1,20 @@
 import { namedTypes as n, builders as b, visit } from "ast-types";
-import recast from "recast";
-import flow from  "flow-parser";
+import flow from "flow-parser";
 import util from 'util';
-const deb = x => (JSON.stringify(x, null,2));
 const ins = x => util.inspect(x, {depth:3});
 
-const code = `
-$a = 4 + 2 , $b = 5 * $a , $print($b); 
-`;
+const codes = [
+    `$a = 4 + 2 , $b = 5 * $a , $print($b)`, // no declarations
+    `const $print = console.log; let $a; let $b; $a = 4 + 2 , $b = 5 * $a , $print($b);`
+];
 
-/*const code = `
-const a = 4;
-
-function tutu(a) {
-    const b = a+1;
-    return b;
-}
-
-tutu(a);
-`;
-*/
-let ast = flow.parse(code);
-visit(ast, {
-    
-    // visitFunction(path) {
-    //     const node = path.node;
-
-    //     console.log('/*----- tutu scope ----*/');
-
-    //     console.log(Object.keys(path.scope.getBindings())); /* [ 'a', 'b' ] */
-
-    //     console.log('/*----- global scope ----*/');
-    //     console.log(ins(path.parent.scope.getBindings())); /* { a: [ NodePath {... } ], tutu: [   NodePath { ...}  ] } */
-
-    //     this.abort();
-
-    //     //this.traverse(path)
-    // },
-    
-
-    
-    visitProgram(path) {
-        const node = path.node;
-
-        console.log(ins(path.scope.getBindings()));
-
-        this.traverse(path)
-    }
-    
+codes.forEach(code => {
+    let ast = flow.parse(code);
+    visit(ast, {
+        visitProgram(path) {
+            console.log(ins(Object.keys(path.scope.getBindings())));
+            this.traverse(path)
+        }        
+    })
 })
 
