5edc2cb97af3487fd8020cde2eba91053792dc2a
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2023-03-03 18:14:10 +0000

    shallow copy

--- eachfield/copyAST.js
+++ eachfield/copyAST.js
@@ -1,10 +1,11 @@
-import { namedTypes as n, builders as b } from "ast-types";
+import { builders as b } from "ast-types";
 import recast from "recast";
-import flow from 'flow-parser';
 import { eachField } from "ast-types";
-
 const deb = x => (JSON.stringify(x, null,2));
 
+/* Let us build a node corresponding to s.t. like: 
+const dupNode = flow.parse(`if (foo) foo()`).body[0];
+console.log(dupNode) */
 const fooId = b.identifier("foo");
 const node = b.ifStatement(
                fooId,
@@ -14,16 +15,13 @@ const node = b.ifStatement(
                 ])
 );
 
-/* Similar to 
-const dupNode = flow.parse(`if (foo) foo()`).body[0];
-console.log(dupNode)
-*/
+// Now let us copy it using eachField to traverse the original
 const copy = {};
 
-// Iterate over all defined fields of an object, including those missing
-// or undefined, passing each field name and effective value (as returned
-// by getFieldValue) to the callback. If the object has no corresponding
-// Def, the callback will never be called.
+// eachField iterates over all defined fields of an object, including those missing
+// or undefined, passing to the callback each field name and effective value (as returned
+// by getFieldValue). If the object has no corresponding
+// Def, the callback will never be called. Here is the code:
 // export function eachField(object, callback, context) {
 //    getFieldNames(object).forEach(function(name) {
 //      callback.call(this, name, getFieldValue(object, name));
@@ -36,11 +34,7 @@ eachField(node, function (name, value) {
   copy[name] = value;
 });
 
-console.log(deb(copy));
+node.test.loc = "Is it a shallow copy?"
+console.log(deb(copy)); // the output shows that it is a shallow copy
 
-console.log(recast.print(copy).code);
-/*
-if (foo) {
-    foo();
-}
-*/
+console.log(recast.print(copy).code); /* if (foo) { foo(); } */
