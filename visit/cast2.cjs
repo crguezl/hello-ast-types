ea47aea112f89881c9c7a81d1bf35fe953050f42
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2023-03-06 13:53:57 +0000

    cast2.cjs

---
+++ visit/cast2.cjs
@@ -0,0 +1,37 @@
+// outputs a compact string representation of the AST. Example:
+//   ✗ node cast.cjs 'if(a) 1; else 2'             
+//   Program(IfStatement(Identifier(a)ExpressionStatement(Literal(1))ExpressionStatement(Literal(2))))
+//   ✗ node cast.cjs 'while (x == 1) {}'             
+//   Program(WhileStatement(BinaryExpression(==()Identifier(x)Literal(1))BlockStatement()))
+const util = require("util");
+const espree = require("espree");
+const astTypes = require("ast-types");
+
+const code =process.argv[2] || "a =1; b = 2;";
+const depth = process.argv[3] || null;
+const ast = espree.parse(code, { ecmaVersion: espree.latestEcmaVersion });
+
+let tree = [];
+let top = () => tree[tree.length - 1];
+
+astTypes.visit(ast, {
+  visitNode: function(path) {
+    let oldTree = tree;
+    tree.push([ path.node.type ]);
+    tree = top();
+    if (path.node.operator) {
+      tree.push(path.node.operator);
+    }
+    this.traverse(path);
+    if (path.node.name) {
+      tree.push(path.node.name);
+    } else if (path.node.value) {
+      tree.push(path.node.value);
+    }
+    oldTree.pop();
+    oldTree.push(tree);
+    tree = oldTree;
+  }
+});
+
+console.log(JSON.stringify(tree[0], null, 2));
