e0a3130948022b9c324e3968e78bbd0f248792b6
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2023-03-13 10:52:47 +0000

    arrow-vs-function-and-this-2.js

--- visit/arrow-vs-function-and-this-2.js
+++ visit/arrow-vs-function-and-this-2.js
@@ -5,6 +5,15 @@ Example illustrating: that:
 1. Arrow functions take their value of "this" from the lexical scope 
    and it is taken from the parent function scope even if the "this" in that scope is undefined.
 2. Functions take their value of "this" from the context object.
+
+When a function is defined inside another function, the value of "this" can change. 
+In particular, when a nested function is defined inside another function, 
+the value of "this" inside the nested function is not automatically inherited from the parent function.
+
+Instead, the value of "this" inside the nested function will depend on how the function is called. 
+If the nested function is called with a specific "this" value using call(), apply(), or bind(), 
+then that value will be used. 
+If not, the value of "this" inside the nested function will default to the global object (in non-strict mode) or undefined (in strict mode).
 */
 let g = {
     myVar: 'g',
@@ -13,7 +22,7 @@ let g = {
         function chuchu() { // Function without a defined "this"
             let nestedObj = {
                 myVar: 'foo',
-                a: () => console.log(this), //  1
+                a: () => console.log("arrow function: ",this), //  1
                 objFunc: function () {
                     console.log(this.myVar); // foo
                     this.a()                 // g
